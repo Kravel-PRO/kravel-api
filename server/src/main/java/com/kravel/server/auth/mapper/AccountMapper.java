13727de10b4a1ccc5d96159e15e22e5baa1fc887
ooeunz <yuns994@gmail.com>
2020-05-29 18:23:33 +0900

    login mybatis

--- server/src/main/java/com/kravel/server/auth/mapper/AccountMapper.java
+++ server/src/main/java/com/kravel/server/auth/mapper/AccountMapper.java
@@ -2,11 +2,12 @@ package com.kravel.server.auth.mapper;
 
 import com.kravel.server.auth.model.Account;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface AccountMapper {
 
-    public Account findByLoginEmail(String loginEmail);
+    public Account findByLoginEmail(@Param("loginEmail") String loginEmail);
 }
