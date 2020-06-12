c90ce662606591e9f962bfed5efe0cc7cb62348e
ooeunz <yuns994@gmail.com>
2020-06-12 22:22:56 +0900

    todo it need more value

--- src/main/java/com/kravel/server/api/article/mapper/CelebrityMapper.java
+++ src/main/java/com/kravel/server/api/article/mapper/CelebrityMapper.java
@@ -13,4 +13,5 @@ import java.util.Map;
 public interface CelebrityMapper {
 
     public List<CelebrityDTO> findAllCelebrities(@Param("param") Map<String, Object> param);
+
 }
--- src/main/java/com/kravel/server/api/article/service/CelebrityService.java
+++ src/main/java/com/kravel/server/api/article/service/CelebrityService.java
@@ -1,11 +1,15 @@
 package com.kravel.server.api.article.service;
 
 import com.kravel.server.api.article.Model.Celebrity;
+import com.kravel.server.api.article.dto.ArticleReviewListDTO;
 import com.kravel.server.api.article.dto.CelebrityDTO;
+import com.kravel.server.api.article.dto.ReviewDTO;
 import com.kravel.server.api.article.mapper.CelebrityMapper;
+import com.kravel.server.api.article.mapper.ReviewMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,9 +17,22 @@ import java.util.Map;
 public class CelebrityService {
 
     @Autowired
+    private ReviewMapper reviewMapper;
+
+    @Autowired
     private CelebrityMapper celebrityMapper;
 
     public List<CelebrityDTO> findAllCelebrities(Map<String, Object> param) throws Exception {
         return celebrityMapper.findAllCelebrities(param);
+    }
+
+    public Map<String, Object> findCelebrityById(Map<String, Object> param) throws Exception {
+        Map<String, Object> result = new HashMap<>();
+
+        List<ArticleReviewListDTO> articleReviewListDTOList = reviewMapper.findAllReviews(param);
+        result.put("reviewList", articleReviewListDTOList);
+
+        // TODO 다른 value 파편도 모아야함.
+        return result;
     }
 }
