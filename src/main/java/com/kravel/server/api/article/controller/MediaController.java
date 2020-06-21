255cac083bd2b5942fcd3d7473b65619cb3b1102
ooeunz <yuns994@gmail.com>
2020-06-21 17:31:30 +0900

    create api admie notice controller

--- src/main/java/com/kravel/server/api/article/controller/MediaController.java
+++ src/main/java/com/kravel/server/api/article/controller/MediaController.java
@@ -4,9 +4,12 @@ import com.kravel.server.api.article.dto.media.ExMediaArticleDTO;
 import com.kravel.server.api.article.dto.media.MediaArticleDTO;
 import com.kravel.server.api.article.dto.media.MediaInfoDTO;
 import com.kravel.server.api.article.dto.media.MediaListDTO;
+import com.kravel.server.api.article.dto.review.ArticleReviewListDTO;
 import com.kravel.server.api.article.service.MediaService;
+import com.kravel.server.api.article.service.ReviewService;
 import com.kravel.server.auth.security.util.jwt.ClaimExtractor;
 import com.kravel.server.common.util.message.ResponseMessage;
+import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -26,6 +29,9 @@ public class MediaController {
 
     @Autowired
     private ClaimExtractor claimExtractor;
+
+    @Autowired
+    private ReviewService reviewService;
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
@@ -82,5 +88,24 @@ public class MediaController {
         return new ResponseMessage(HttpStatus.OK, result);
     }
 
+    @GetMapping("/{mediaId}/reviews")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseMessage findMediaReviewsById(@PathVariable("mediaId") long mediaId,
+                                                @RequestParam(value = "offset", defaultValue = "0") int offset,
+                                                @RequestParam(value = "max", defaultValue = "6") int max,
+                                                @RequestParam(value = "sort", defaultValue = "CREATE_DE") String sort,
+                                                @RequestParam(value = "order", defaultValue = "DESC") String order,
+                                                Authentication authentication) throws Exception {
 
+        Map<String, Object> param = new HashMap<>();
+        param.put("mediaId", mediaId);
+        param.put("offset", offset);
+        param.put("max", max);
+        param.put("sort", sort);
+        param.put("order", order);
+
+        List<ArticleReviewListDTO> articleReviewListDTOs = reviewService.findAllReviews(param);
+        return new ResponseMessage(HttpStatus.OK, articleReviewListDTOs);
+    }
 }
