10fafa94ac60ae13414af2d867f2daac4395a0c6
ooeunz <yuns994@gmail.com>
2020-06-21 16:58:47 +0900

    create media places api

--- src/main/java/com/kravel/server/api/article/controller/MediaController.java
+++ src/main/java/com/kravel/server/api/article/controller/MediaController.java
@@ -1,8 +1,11 @@
 package com.kravel.server.api.article.controller;
 
+import com.kravel.server.api.article.dto.media.ExMediaArticleDTO;
+import com.kravel.server.api.article.dto.media.MediaArticleDTO;
 import com.kravel.server.api.article.dto.media.MediaInfoDTO;
 import com.kravel.server.api.article.dto.media.MediaListDTO;
 import com.kravel.server.api.article.service.MediaService;
+import com.kravel.server.auth.security.util.jwt.ClaimExtractor;
 import com.kravel.server.common.util.message.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,9 @@ public class MediaController {
 
     @Autowired
     private MediaService mediaService;
+
+    @Autowired
+    private ClaimExtractor claimExtractor;
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
@@ -53,4 +59,28 @@ public class MediaController {
         MediaInfoDTO result = mediaService.findMediaInfoById(param);
         return new ResponseMessage(HttpStatus.OK, result);
     }
+
+    @GetMapping("/{mediaId}/places")
+    @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ResponseMessage findMediaArticlesById(@PathVariable("mediaId") long mediaID,
+                                                 @RequestParam(value = "offset", defaultValue = "0") int offset,
+                                                 @RequestParam(value = "max", defaultValue = "6") int max,
+                                                 @RequestParam(value = "sort", defaultValue = "CREATE_DE") String sort,
+                                                 @RequestParam(value = "order", defaultValue = "DESC") String order,
+                                                 Authentication authentication) throws Exception {
+
+        Map<String, Object> param = new HashMap<>();
+        param.put("mediaId", mediaID);
+        param.put("offset", offset);
+        param.put("max", max);
+        param.put("sort", sort);
+        param.put("order", order);
+        param.put("langu", claimExtractor.getLangu(authentication));
+
+        List<MediaArticleDTO> result = mediaService.findMediaArticlesById(param);
+        return new ResponseMessage(HttpStatus.OK, result);
+    }
+
+
 }
